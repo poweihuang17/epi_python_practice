fd873ffcf2e7d69d4aa648a9b973795892a794bc
Po-wei Huang <poweihuang17@gmail>
2017-12-06 13:20:35 +0800

    Check in code of isbn. Didn't I checked in before?

--- Hash/isbn_12_3.py
+++ Hash/isbn_12_3.py
@@ -1,5 +1,8 @@
+import collections
+
 class LRUCache:
 	def __init__(self,capacity):
+		#Why we use order dictionary here?
 		self._isbn_price_table = collections.OrderedDict()
 		self._capacity= capacity
 
@@ -8,3 +11,24 @@ class LRUCache:
 			return False, None
 		price= self._isbn_price_table[isbn]
 		return True, price
+
+	def insert(self, isbn, price):
+		if isbn in self._isbn_price_table:
+			#pop and insert again in order to update the order.
+			price=self._isbn_price_table.pop(isbn)
+		elif self._capacity <= len(self._isbn_price_table):
+			#FIFO out
+			self._isbn_price_table.popitem(last=False)
+
+		self._isbn_price_table[isbn]=price
+
+	def erase(self, isbn):
+		return self._isbn_price_table.pop(isbn,None) is not None
+
+cache= LRUCache(3)
+cache.insert("12345", 12.0)
+cache.insert("23456", 30.0)
+print cache.lookup("12345")
+
+print cache.erase("12345")
+
