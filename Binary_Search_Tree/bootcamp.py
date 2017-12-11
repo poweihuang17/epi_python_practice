6239ea0f5b413cc68e8a26ff8b732525ed00aeb0
Po-wei Huang <poweihuang17@gmail>
2017-12-11 14:22:11 +0800

    Start to use bintrees.

--- Binary_Search_Tree/bootcamp.py
+++ Binary_Search_Tree/bootcamp.py
@@ -1,6 +1,20 @@
+import bintrees
+
 class BSTNode:
 
 	def __init__(self, data=None, left=None, right=None):
 		self.data, self.left,self.right= data, left, right
+
+
+t = bintrees.RBTree([ (5,'Alfa'), (2, 'Bravo'), (7,'Charlie'), (3,'Delta'), (6, 'Echo')])
+print t[2]
+
+print (t.min_item(), t.max_item())
+
+t.insert(9, 'Golf')
+
+print (t)
+
+print (t.min_key(), t.max_key())
 
 
