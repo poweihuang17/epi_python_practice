fb87aa229949adbb674bce37670fc9f4756a6434
Po-wei Huang <poweihuang17@gmail>
2017-12-12 14:49:48 +0800

    Finish solution 2 of prob 15.4.

--- Recursion/generate_power_set_15_4.py
+++ Recursion/generate_power_set_15_4.py
@@ -1,3 +1,5 @@
+import math
+
 def generate_power_set_1(input_set):
 
 	def directed_power_set(to_be_selected, selected_so_far):
@@ -18,7 +20,22 @@ def generate_power_set_1(input_set):
 
 
 power_set = generate_power_set_1([1,2,3,4])
-print len(power_set)
-print power_set
+print(len(power_set))
+print(power_set)
 
-#def generate_power_set_2(input_set):
+def generate_power_set_2(S):
+	power_set=[]
+	for int_for_subset in range(1<<len(S)):
+		bit_array=int_for_subset
+		subset=[]
+		while bit_array:
+			subset.append(S[int(math.log2(bit_array & ~(bit_array-1)))])
+			bit_array &= bit_array-1
+		power_set.append(subset)
+
+	return power_set
+
+power_set_2=generate_power_set_2([1,2,3,4])
+print(len(power_set_2))
+print(power_set_2)
+
