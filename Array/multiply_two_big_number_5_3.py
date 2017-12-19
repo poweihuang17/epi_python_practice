2db0dac5dbf6a1c3f2cf3c9f28af2fd36449d3dd
Po-wei Huang <poweihuang17@gmail>
2017-12-19 11:49:06 +0800

    Finish my version of 5.3. Still don't know why do we need an (or[0]) and len(result) at the next. The solution is weird. Maybe we need to go to forum to ask it.

---
+++ Array/multiply_two_big_number_5_3.py
@@ -0,0 +1,18 @@
+def multiply(num1, num2):
+	sign=-1 if (num1[0]<0)^(num2[0]<0) else 1
+	num1[0], num2[0]=abs(num1[0]), abs(num2[0])
+
+	result=[0]*(len(num1)+len(num2))
+	for i in reversed(range(len(num1))):
+		for j in reversed(range(len(num2))):
+			result[i+j+1]+= num1[i]*num2[j]
+			result[i+j]+=result[i+j+1]//10
+			result[i+j+1]%=10
+
+	result= result[ next( (i for i , x in enumerate(result) if x!=0)):]
+
+	return [sign*result[0]] +result[1:]
+
+
+
+print multiply([-1,3,5], [2,3,4])
