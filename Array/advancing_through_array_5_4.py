e1fdfb773ba5af345c8208f059f200e5063479ed
Po-wei Huang <poweihuang17@gmail>
2017-12-19 13:19:58 +0800

    Finish the variant of 5.4. Right now, I could output the minimum step simultaneously. However, this is a O(n^2) solution for the minimum stage. The solution of leetcode jump II has a O(n) solution.

--- Array/advancing_through_array_5_4.py
+++ Array/advancing_through_array_5_4.py
@@ -3,14 +3,31 @@ def advancing_through_array(A):
 	# output of A: bool
 
 	farthest=0
+	dp=[0]+[float('inf')]*(len(A)-1)
+
 	for index, item in enumerate(A):
+
 		if index > farthest:
-			return False
+			 success=False
+			 break
+
+		for j in range(1,item+1):
+			if (index+j) >= len(A):
+				break
+			dp[index+j]=min(dp[index+j], dp[index]+1)
+
+		#print dp
 
 		farthest= max(farthest, index+item)
 
 		if farthest==len(A)-1:
-			return True
+			success=True
+			return (success, dp[len(A)-1])
+
+
+
+
+	return (success, dp[len(A)-1])
 
 print advancing_through_array([3,3,1,0,2,0,1])
 print advancing_through_array([3,2,0,0,2,0,1])
